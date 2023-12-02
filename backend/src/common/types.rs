4875c43a3cf6ffee8768a2169b2360e9722e2c1e
Sai Kumar <ksai44576@gmail.com>
2023-12-02 11:47:45 +0530

    refactor `SignupRequest`

--- backend/src/common/types.rs
+++ backend/src/common/types.rs
@@ -1,8 +1,23 @@
 use serde::Deserialize;
 
-#[derive(Debug, Clone, Deserialize)]
+#[cfg(test)]
+use serde::Serialize;
+
+#[derive(Clone, Debug, Deserialize)]
+#[cfg_attr(test, derive(Serialize))]
 pub struct SignupRequest {
     pub username: String,
     pub email: String,
     pub password: String,
+}
+
+#[cfg(test)]
+impl SignupRequest {
+    pub fn new(username: String, email: String, password: String) -> Self {
+        Self {
+            username,
+            email,
+            password,
+        }
+    }
 }
